0fab6bc2cae7fafd018fd6d530318c0f995e3ceb
unteez <49522119+unteez@users.noreply.github.com>
2019-04-11 15:13:20 +0300

    Create uzduotis.js

---
+++ uzduotis.js
@@ -0,0 +1,3 @@
+//Duotas kintamasis 'x' kurio reiksme lygu '5' ir masyvo elementai: [5,8,9,14,20]
+// Parasyti cikla kuris pridetu antro elememento skaiciu prie 'x';
+// Gauta atsakyma isvesti konsoleje. Atsakyma isvesti pasibaigus ciklo blokui
